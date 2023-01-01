70b307a7aed72d826f36c7675462b93383390d9d
ViraZhuhan <virazhuhan@gmail.com>
2023-01-01 10:43:05 +0200

    Закончене ДЗ

--- js/modal.js
+++ js/modal.js
@@ -13,14 +13,3 @@
   }
 })();
 
-(() => {
-  document.querySelector('.js-speaker-form').addEventListener('submit', e => {
-      e.preventDefault();
-
-      new FormData(e.currentTarget).forEach((value, name) =>
-          console.log(`${name}: ${value}`),
-      );
-
-      e.currentTarget.reset();
-  });
-})();
